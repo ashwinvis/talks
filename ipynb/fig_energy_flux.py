953ac2b3e633209a49707eb9f355728b38283e7a
Ashwin Vishnu <avmo@kth.se>
2018-03-06 10:44:56 +0100

    Some last min modifications

--- ipynb/fig_energy_flux.py
+++ ipynb/fig_energy_flux.py
@@ -7,6 +7,7 @@ import numpy as np
 
 matplotlib.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
 plt.rc('text', usetex=True)
+plt.rc('figure', dpi=300)
 
 fig = plt.figure(figsize=(10,5))
 fig.subplots_adjust(right=5/10)
@@ -23,20 +24,21 @@ ax.add_collection(p)
 arror_r = pi / 2**1.5
 ax.arrow(arror_r, arror_r, 0.3, 0.3, head_width=0.05, head_length=0.1, fc='k', ec='k')
 tex = r'$\Pi(k_\rho) = \sum\limits_{k_\rho \geq \sqrt{k_x^2 + k_y^2}} T(k_x, k_y)$'
-ax.text(0.9, 1.2, tex, fontsize=14, va='bottom', rotation=45)
+ax.text(0.9, 1.2, tex, fontsize=15, va='bottom', rotation=45)
 
 # plt.rc('text', usetex=False)
 tex = r'''$T=T_K + T_A$
 $T_K=-\frac{1}{2}ik_i(\hat{u_j}\widehat{u_iu_j}^* - \hat{u_j}^*\widehat{u_iu_j})$
 $T_A=-\frac{1}{2}ik_i(\hat{\phi}\widehat{u_i \phi}^* - \hat{\phi}^*\widehat{u_i\phi})$'''
-ax.text(pi + 0.2, pi/2, tex, fontsize=18)
+ax.text(pi + 0.2, pi/2, tex, fontsize=20)
 
 tex = r'''$\Pi=\Pi_K + \Pi_A$
 $\Pi=\Pi_{VVV} + \Pi_{VVW} + \Pi_{VWW} + \Pi_{WWW}$'''
 
-ax.text(pi + 0.2, pi/4, tex, fontsize=18)
-ax.set_xlabel(r'$k_x$')
-ax.set_ylabel(r'$k_y$')
+ax.text(pi + 0.2, pi/4, tex, fontsize=20)
+ax.set_xlabel(r'$k_x$', fontsize=15)
+ax.set_ylabel(r'$k_y$', fontsize=15)
 ax.set_ylim(0, pi)
 ax.set_xlim(0, pi)
-plt.show()
+# plt.show()
+plt.savefig('./fig/flow_flux_schem.png')
